240efcd1cf6b9a42ed6cde2107af0d4d0b04ab9b
mjrfornari <mjrfornari@gmail.com>
2018-08-20 21:04:28 -0300

    asd
    asd

--- src/App.js
+++ src/App.js
@@ -138,9 +138,9 @@ class App extends Component {
 					<Lifebar playerLives={this.state.playerLives} />
 					<Scoreboard score={this.state.score} />
                 </content>
-                <button className="btnLeft" onclick={socket.emit('turnRight')}></button>
-                <button className="btnShoot" onclick={socket.emit('shoot')}></button>
-                <button className="btnRight" onclick={socket.emit('turnLeft')}></button>
+                <button className="btnLeft" onClick={socket.emit('turnRight')}></button>
+                <button className="btnShoot" onClick={socket.emit('shoot')}></button>
+                <button className="btnRight" onClick={socket.emit('turnLeft')}></button>
             </div>
         )
     }
